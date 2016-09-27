4b35ec71f510443174b7c23c73357cbce4cbe86b
cjspiece <cjspiece@svsu.edu>
2016-09-27 17:24:08 -0400

    Added school logo to the activities

--- app/src/main/java/com/example/cjspiece/csis/CourseGridActivity.java
+++ app/src/main/java/com/example/cjspiece/csis/CourseGridActivity.java
@@ -3,13 +3,32 @@ package com.example.cjspiece.csis;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CourseGridActivity extends AppCompatActivity {
+
+    private boolean chartShown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_grid);
+    }
+
+    public void showPlanChart(View view) {
+
+        if(!chartShown) {
+            chartShown = true;
+            ImageView imgView = (ImageView) findViewById(R.id.plan_img);
+            imgView.setVisibility(View.VISIBLE);
+        }
+        else {
+            chartShown = false;
+            ImageView imgView = (ImageView) findViewById(R.id.plan_img);
+            imgView.setVisibility(View.GONE);
+        }
+
     }
 
     public void backToMain(View view) {
